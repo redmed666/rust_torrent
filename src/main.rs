02b94e063a7df07e7ec17ecade920989c7e61a44
med <khemir.med@gmail.com>
2019-05-31 20:15:46 +0200

    Removed pub mod magnet because it was redundant

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,11 @@ fn test_error() -> Result<String, String> {
 fn main() {
     let test = String::from("test");
     let magnet_link = String::from("magnet:?xt=urn:btih:4ddb6ed03f413ef34718111697573c839ed30eb9&dn=Its+Always+Sunny+in+Philadelphia+Season+1%2C+2%2C+3%2C+4%2C+5+%26amp%3B+6+%2B+&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969");
-    let mag = match magnet::magnet::from_string(magnet_link) {
+    let mag = match magnet::from_string(magnet_link) {
         Ok(v) => v,
         Err(e) => {
             println!("{}", e);
-            magnet::magnet::Magnet::new()
+            magnet::Magnet::new()
         }
     };
     println!("{}", mag.header);
